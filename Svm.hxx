b50ecc030a259b017ba1be1f20439bea2a2f7d8e
Samuel <181611094+svespalec@users.noreply.github.com>
2025-12-30 15:22:46 -0800

    Define core SVM bits

--- Svm.hxx
+++ Svm.hxx
@@ -3,6 +3,17 @@
 #include <ntddk.h>
 #include <intrin.h>
 
+// MSRs
+#define MSR_EFER        0xC0000080
+#define MSR_VM_CR       0xC0010114
+#define MSR_VM_HSAVE_PA 0xC0010117
+
+// EFER bits
+#define EFER_SVME ( 1ULL << 12 )
+
+// VM_CR bits
+#define VM_CR_SVMDIS ( 1ULL << 4 )
+
 namespace Svm {
   BOOLEAN IsSupported( );
   BOOLEAN IsDisabled( );
