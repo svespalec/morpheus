a8748e5c64dedcce9079db0f04af1fa354fe88dc
Samuel <181611094+svespalec@users.noreply.github.com>
2025-12-30 14:55:23 -0800

    Test serial port output (control registers)

--- Driver.cxx
+++ Driver.cxx
@@ -1,4 +1,5 @@
 #include "Svm.hxx"
+#include "Serial.hxx"
 
 VOID DriverUnload( PDRIVER_OBJECT DriverObject ) {
   UNREFERENCED_PARAMETER( DriverObject );
@@ -12,6 +13,10 @@ NTSTATUS DriverEntry( PDRIVER_OBJECT DriverObject, PUNICODE_STRING RegistryPath 
   DriverObject->DriverUnload = DriverUnload;
 
   DbgPrint( "Loaded" );
+
+  Serial::PrintHex( "CR0", __readcr0( ) );
+  Serial::PrintHex( "CR3", __readcr3( ) );
+  Serial::PrintHex( "CR4", __readcr4( ) );
 
   return STATUS_SUCCESS;
 }
--- Svm.hxx
+++ Svm.hxx
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <ntddk.h>
+#include <intrin.h>
 
 namespace Svm {
   BOOLEAN IsSupported( );
