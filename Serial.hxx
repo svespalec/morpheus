663ada6af6a4b8b7d1f04b6d9a0d5f62875489a8
Samuel <181611094+svespalec@users.noreply.github.com>
2025-12-30 17:01:35 -0800

    Add detailed comments regarding SVM

--- Serial.hxx
+++ Serial.hxx
@@ -8,7 +8,7 @@ namespace Serial {
   inline void Init() {
     __outbyte( COM1 + 1, 0x00 ); // Disable interrupts
     __outbyte( COM1 + 3, 0x80 ); // Enable DLAB
-    __outbyte( COM1 + 0, 0x01 ); // 115200 baud
+    __outbyte( COM1 + 0, 0x01 ); // 115200 baud (speed)
     __outbyte( COM1 + 1, 0x00 );
     __outbyte( COM1 + 3, 0x03 ); // 8N1
     __outbyte( COM1 + 2, 0xC7 ); // Enable FIFO
--- Svm.cxx
+++ Svm.cxx
@@ -1,5 +1,7 @@
 #include "Svm.hxx"
 
+// Checks if CPU even has SVM hardware capability (AMD + SVM feature bit)
+// This will return true even if SVM is disabled in BIOS as it only checks capabilities
 BOOLEAN Svm::IsSupported() {
   int cpuInfo[ 4 ];
 
@@ -25,6 +27,10 @@ BOOLEAN Svm::IsSupported() {
   return TRUE;
 }
 
+// Checks if BIOS has forcibly locked out SVM (VM_CR.SVMDIS bit)
+// This is different from SVM just not being enabled.
+// It means the BIOS explicitly prevents SVM usage which is common
+// on enterprise machines where IT locks down virtualization.
 BOOLEAN Svm::IsDisabled() {
   UINT64 vmCr = __readmsr( MSR_VM_CR );
 
@@ -37,6 +43,9 @@ BOOLEAN Svm::IsDisabled() {
 }
 
 void Svm::Enable() {
+  // EFER (Extended Feature Enable Register) controls CPU features like SVM, long mode, NX, etc.
+  // It's basically just a bunch of on/off switches for extended CPU functionality.
+  // The BIOS enables SVM capability, but we must set EFER.SVME to actually use it pretty much
   UINT64 efer = __readmsr( MSR_EFER );
   efer |= EFER_SVME;
 
